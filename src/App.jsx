05fe5861149f01e3ba3012b2a464c578cb93dcea
sabz <rgsabareesh@gmail.com>
2022-12-03 05:27:56 +0530

    useQuery add with  loading and error check and dev tool added

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
 import { Routes, Route, Link } from 'react-router-dom';
 
+import { QueryClientProvider, QueryClient } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
 import Home from './components/Home';
 import Superheroes from './components/Superheroes';
 import RQSuperheros from './components/RQSuperheroes';
-
-import './App.css';
 import Navbar from './components/Navbar';
+import './App.css';
+
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
+
         <Route path='/superheroes' element={<Superheroes />} />
         <Route path='/rqsuperheroes' element={<RQSuperheros />} />
       </Routes>
-    </>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 export default App;
